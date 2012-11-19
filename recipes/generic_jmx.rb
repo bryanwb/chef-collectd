0ca9f74df3b963cf9722b79989994fc04f33af8a
Bryan Berry <bryan.berry@gmail.com>
2012-11-19 13:14:31 +0100

    templates work with tomcat queries

--- recipes/generic_jmx.rb
+++ recipes/generic_jmx.rb
@@ -1,39 +1,33 @@
+java_cflags = [
+               "--enable-java",
+               "JAR=#{node['java']['java_home']}/bin/jar",
+               "JAVAC=#{node['java']['java_home']}/bin/javac"
+              ]
 
-%w{ java-1.6.0-openjdk java-1.6.0-openjdk-devel }.each do |pkg|
-  package pkg do
-    notifies :run, resources("ruby_block[reset-java-alternatives]"), :delayed
+java_cflags.each do |flag|
+  unless  node[:collectd][:cflags].include? flag
+    node[:collectd][:cflags] << flag
   end
-end
-
-ruby_block "reset-java-alternatives" do
-  block do
-    ::Dir["#{node['java']['java_home']}/bin/*"].each do |bin_cmd|
-      cmd_name = ::Dir.basename bin_cmd
-      java_home = node['java']['java_home']
-      cmd = Chef::ShellOut.new(
-                               %Q[ update-alternatives --install /usr/bin/#{cmd_name} #{cmd_name} #{bin_cmd};
-                                   update-alternatives --set #{cmd_name} #{bin_cmd} ]
-                               )
-    end
-  end
-  action :nothing
 end
 
 bash "recompile-collectd" do
   code <<-EOF
   cd /opt/collectd
-  ./configure
-  make 
+  ./configure #{node[:collectd][:cflags].join(' ')}
+  make
   make install
 EOF
   not_if { ::File.exists? "#{node[:collectd][:plugin_dir]}/java.so" }
 end
 
-
-
-
 template "/etc/collectd/plugins/generic_jmx.conf" do
   source "generic_jmx.conf.erb"
+  mode "0644"
+  notifies :restart, resources(:service => "collectd")
+end
+
+template "/etc/collectd/plugins/generic-jmx-types.conf" do
+  source "generic_jmx_types.db.erb"
   mode "0644"
   notifies :restart, resources(:service => "collectd")
 end
