a473376513755abe9c4e9acd280a44f405d6a3a2
Bryan Berry <bryan.berry@gmail.com>
2012-11-21 11:55:06 +0100

    fixup the path for JAVA_HOME w/ extra '/'

--- recipes/generic_jmx.rb
+++ recipes/generic_jmx.rb
@@ -1,19 +1,12 @@
-java_cflags = [
-               "--enable-java",
-               "JAR=#{node['java']['java_home']}/bin/jar",
-               "JAVAC=#{node['java']['java_home']}/bin/javac"
-              ]
-
-java_cflags.each do |flag|
-  unless  node[:collectd][:cflags].include? flag
-    node[:collectd][:cflags] << flag
-  end
-end
-
+# the configure script fails unless JAVA_HOME terminates with "/"
+# super weird!
+java_home = ENV['JAVA_HOME'].dup
+java_home = "#{java_home}/" unless java_home[-1] == '/'
+  
 bash "recompile-collectd" do
   code <<-EOF
   cd /opt/collectd
-  ./configure #{node[:collectd][:cflags].join(' ')}
+  ./configure #{node[:collectd][:cflags].join(' ')} --with-java=#{java_home}
   make
   make install
 EOF
@@ -26,7 +19,7 @@ template "/etc/collectd/plugins/generic_jmx.conf" do
   notifies :restart, resources(:service => "collectd")
 end
 
-template "/etc/collectd/plugins/generic-jmx-types.conf" do
+template "/etc/collectd/plugins/generic_jmx_types.db" do
   source "generic_jmx_types.db.erb"
   mode "0644"
   notifies :restart, resources(:service => "collectd")
