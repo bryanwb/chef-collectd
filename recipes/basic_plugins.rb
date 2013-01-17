2583a1604abd6acf7f0085ef903506c537af0046
Tim Smith <tsmith@llnw.com>
2013-01-17 11:01:20 -0800

    Rename basic_plugins to base_plugins and install via attribute

--- recipes/basic_plugins.rb
+++
@@ -1,6 +0,0 @@
-# This recipe installs the most frequently used plugins without
-# any special configuration
-
-%w{cpu interface load memory network df disk}.each do |plugin|
-  collectd_plugin plugin
-end
