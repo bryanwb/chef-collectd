0e94074e6d8b986affa2ff1c8a2d8141f2c6fffb
Tim Smith <tsmith@llnw.com>
2013-03-07 14:23:45 -0800

    Set attribute precedence to for Chef 11

--- recipes/source.rb
+++ recipes/source.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,9 +19,9 @@
 
 include_recipe "ark"
 
-node[:collectd][:base_dir] = "/opt/collectd"
-node[:collectd][:plugin_dir] = "/opt/collectd/lib/collectd"
-node[:collectd][:types_db] = ["/opt/collectd/share/collectd/types.db"]
+node.set['collectd']['base_dir'] = "/opt/collectd"
+node.set['collectd']['plugin_dir'] = "/opt/collectd/lib/collectd"
+node.set['collectd']['types_db'] = ["/opt/collectd/share/collectd/types.db"]
 
 if platform_family? "debian"
   package "perl-modules"
