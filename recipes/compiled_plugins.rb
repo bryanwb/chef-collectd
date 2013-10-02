2cf633f1ccd35326ad02fa0f97d657949a8ee35a
Tim Smith <tsmith@llnw.com>
2013-10-02 11:35:42 -0700

    Remove yumrepo cookbook include for postgresql

--- recipes/compiled_plugins.rb
+++ recipes/compiled_plugins.rb
@@ -23,7 +23,6 @@
 node[:collectd][:compiled_plugins].each do |plugin|
   case plugin
   when /postgres/  # cuz everybody spells it different
-      include_recipe "yumrepo::postgresql"
     if platform_family? 'rhel'
       pkg_version = node['postgresql']['version'].split('.').join('')
       package "postgresql#{pkg_version}-devel"
