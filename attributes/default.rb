644f2f482d4b973b3718e1b613b7388f434471b4
Tim Smith <tsmith@llnw.com>
2013-10-02 11:32:09 -0700

    Install collectd 5.4.0, install via source on RHEL and package on Debian/Ubuntu

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,36 +17,40 @@
 # limitations under the License.
 #
 
-default['collectd']['base_dir'] = "/var/lib/collectd"
-default['collectd']['plugin_dir'] = "/usr/lib/collectd"
-default['collectd']['types_db'] = ["/usr/share/collectd/types.db"]
+default['collectd']['base_dir'] = '/var/lib/collectd'
+default['collectd']['plugin_dir'] = '/usr/lib/collectd'
+default['collectd']['types_db'] = [ '/usr/share/collectd/types.db' ]
 default['collectd']['interval'] = 10
 default['collectd']['read_threads'] = 5
-default['collectd']['install_method'] = "package"
-default['collectd']['collectd_web']['path'] = "/srv/collectd_web"
-default['collectd']['collectd_web']['hostname'] = "collectd"
-default['collectd']['version'] = '5.2.1'
+case node['platform_family']
+when 'debian'
+  default['collectd']['install_method'] = 'package'
+else
+  default['collectd']['install_method'] = 'source'
+end
+default['collectd']['collectd_web']['path'] = '/srv/collectd_web'
+default['collectd']['collectd_web']['hostname'] = 'collectd'
+default['collectd']['version'] = '5.4.0'
 default['collectd']['source_url'] = "http://collectd.org/files/collectd-#{node['collectd']['version']}.tar.gz"
-default['collectd']['checksum'] = '55fa3659218cafc57175fe7b9a67fff71519280ad02f8830e78e6fb642a3dbc9'
+default['collectd']['checksum'] = 'c434548789d407b00f15c361305766ba4e36f92ccf2ec98d604aab2a46005239'
 default['collectd']['base_plugins'] = %w{cpu interface load memory network df disk}
 
-default['collectd']['graphite']['host'] = "localhost"
+default['collectd']['graphite']['host'] = 'localhost'
 default['collectd']['graphite']['port'] = 2003
-default['collectd']['graphite']['prefix'] = "collectd."
-default['collectd']['graphite']['postfix'] = ""
-default['collectd']['graphite']['escape_character'] = "_"
+default['collectd']['graphite']['prefix'] = 'collectd.'
+default['collectd']['graphite']['postfix'] = ''
+default['collectd']['graphite']['escape_character'] = '_'
 default['collectd']['graphite']['store_rates'] = false
 default['collectd']['graphite']['separate_instances'] = false
-default['collectd']['compiled_plugins'] = []
+default['collectd']['compiled_plugins'] = ['libcurl','python']
 default['collectd']['cflags'] = []
 
-default['collectd']['postgresql']['host'] = "localhost"
+default['collectd']['postgresql']['host'] = 'localhost'
 default['collectd']['postgresql']['port'] = 5432
-default['collectd']['postgresql']['username'] = ""
-default['collectd']['postgresql']['password'] = ""
+default['collectd']['postgresql']['username'] = ''
+default['collectd']['postgresql']['password'] = ''
 
 default['collectd']['jmx'] = { 'user' => '', 'password' => '' }
-default['collectd']['haproxy']['stats_socket'] = "/var/lib/haproxy/stats"
+default['collectd']['haproxy']['stats_socket'] = '/var/lib/haproxy/stats'
 
-default['collectd']['syslog']['log_level'] = "info"
-
+default['collectd']['syslog']['log_level'] = 'info'
